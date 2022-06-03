01900757fff1d90438ed2f1b588e4f33a44f3f54
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2022-06-03 10:15:35 +0200

    converted base layers to geojson and loaded

--- map.js
+++ map.js
@@ -1,5 +1,24 @@
+function load(path) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', path);
+    httpRequest.send();
+    console.log(httpRequest.responseText);
+    return httpRequest.responseText;
+}
+
 function create_map() {
-    var map = L.map('map').setView([44.420, 8.800], 10);
+    var map = L.map('map').setView([44.420, 8.800], 10).setMaxBounds([[45, 6], [41, 11]]).setMinZoom(8);
+
+    function geojson_layer(path, style = {}, pane = "default", zIndex = 400) {
+        map.createPane(pane);
+        map.getPane(pane).style.ZIndex = zIndex;
+        $.getJSON(path, (json) =>
+            L.geoJSON(json, {
+                style: style,
+                pane: pane
+            }).addTo(map)
+        );
+    }
 
     /*var wmsLayer = L.tileLayer.wms('https://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?', {
         layers: 'GEBCO_LATEST_SUB_ICE_TOPO'
@@ -15,20 +34,31 @@ function create_map() {
     }).addTo(map);
 
 
-    L.geoJSON(json_LineadiCosta, {
-        style: {
-            opacity: 1,
-            color: 'rgba(27,30,33,1.0)',
-            dashArray: '',
-            lineCap: 'butt',
-            lineJoin: 'miter',
-            weight: 2.0,
-            fill: true,
-            fillOpacity: 0.3,
-            fillColor: 'rgba(211,205,205,1.0)',
-            interactive: false,
-        }
-    }).addTo(map);
+    geojson_layer("./data/LineadiCosta.geojson", {
+        opacity: 1,
+        color: 'rgba(27,30,33,1.0)',
+        weight: 2.0,
+        fill: true,
+        fillOpacity: 0.3,
+        fillColor: 'rgba(211,205,205,1.0)',
+        interactive: false
+    }, "lineadicosta_pane", 401);
+
+    geojson_layer("./data/PiattaformaContinentale.geojson", {
+        opacity: 1,
+        color: 'rgba(30,190,190,1.0)',
+        weight: 2.0,
+        fill: false,
+        interactive: false
+    }, "piattaformacontinentale_pane", 401);
+
+    geojson_layer("./data/Areasimulazione.geojson", {
+        opacity: 1,
+        color: 'rgba(250,30,30,1.0)',
+        weight: 2.0,
+        fill: false,
+        interactive: false
+    }, "areasimulazione_pane", 403);
 
 }
 
